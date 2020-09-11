7e770d1bfc90c6215ac5698155eebd51b802a80b
Milan938 <23mboga@athenian.org>
2020-09-11 00:04:24 +0000

    Fixed errors and finished

--- divisibility_check.py
+++ divisibility_check.py
@@ -2,8 +2,8 @@
 #
 for x in range(2,51):
     print(x)
-remainder = (x%3)
-if remainder is 0:
+    remainder = (x%3)
+    if remainder == 0:
      print("X is divisible by 3")
-else:
-    print("X is not divisible by 3")
+    else:
+     print("X is not divisible by 3")
